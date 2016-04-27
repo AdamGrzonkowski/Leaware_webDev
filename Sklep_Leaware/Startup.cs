8a503ca1e5ca9433f2a4cd48265d16e2a88f2311
Adam Seweryn Grzonkowski <Adam Seweryn Grzonkowski>
2016-04-27 18:18:30 +0200

    First commit

---
+++ Sklep_Leaware/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sklep_Leaware.Startup))]
+namespace Sklep_Leaware
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
