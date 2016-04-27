bdba7039690f71bfa902e86a9414ae1aea518081
adamg <adamg@DESKTOP-U886VAQ>
2016-04-27 18:39:09 +0200

    Przeniesienie modeli.

--- Sklep_Leaware/App_Start/IdentityConfig.cs
+++ Sklep_Leaware/App_Start/IdentityConfig.cs
@@ -10,7 +10,7 @@ using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security;
-using Sklep_Leaware.Models;
+using SL.Model.Models;
 
 namespace Sklep_Leaware
 {
--- Sklep_Leaware/Controllers/AccountController.cs
+++ Sklep_Leaware/Controllers/AccountController.cs
@@ -8,7 +8,7 @@ using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
-using Sklep_Leaware.Models;
+using SL.Model.Models;
 
 namespace Sklep_Leaware.Controllers
 {
