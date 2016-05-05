a19fdd052d75cd528f80c07cea426dd293081a33
adamg <adamg@DESKTOP-U886VAQ>
2016-05-05 18:45:33 +0200

    Klucze obce.

--- SL.Core/Domain/Orders/Cart.cs
+++ SL.Core/Domain/Orders/Cart.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,6 +17,7 @@ namespace SL.Core.Domain.Orders
         public long BookId { get; set; }
         public int Count { get; set; }
         public System.DateTime DateCreated { get; set; }
+        [ForeignKey("BookId")]
         public virtual Books Books { get; set; }
     }
 }
--- SL.Core/Domain/Orders/OrderDetail.cs
+++ SL.Core/Domain/Orders/OrderDetail.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,7 +15,9 @@ namespace SL.Core.Domain.Orders
         public long BookId { get; set; }
         public int Quantity { get; set; }
         public decimal UnitPrice { get; set; }
+        [ForeignKey("BookId")]
         public virtual Books Books { get; set; }
+        [ForeignKey("OrderId")]
         public virtual Order Order { get; set; }
     }
 }
